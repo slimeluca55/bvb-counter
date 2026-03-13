def02b2971e75347b3859cd973f714044dfd31f3
slimeluca55 <slimeluca55@gmail.com>
2026-03-13 11:53:40 +0100

    Add bot.py to calculate days since last championship

---
+++ bot.py
@@ -0,0 +1,8 @@
+from datetime import date
+
+start = date(2012, 4, 21)
+today = date.today()
+
+days = (today - start).days
+
+print(f"Tag {days} ohne Bundesliga-Meisterschaft für Borussia Dortmund 🟡⚫ #BVB")
